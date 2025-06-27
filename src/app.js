fad6a15c7e38cb351ab8dbc27f5efacc835a804b
kranjeet-da <k.ranjeetkumar798@gmail.com>
2025-06-27 14:53:29 +0530

    routing related changes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use
+app.use("/hello/2", (req, res) =>{
+    res.send("Abracadabra");
+});
+
+app.use("/hello", (req, res) =>{
+    res.send("Hello hello hello");
+});
+
+app.use("/test",(req, res)=>{
+    res.send("Hello from server!");
+});
+
+app.use("/", (req, res) =>{
+    res.send("From server end");
+});
 
 app.listen(3000, () => {
     console.log("Server is successfuly running on port 3000...")
